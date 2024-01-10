4750f053b35c008eb4efdd699a068988204478e1
l9o <ertye9020@gmail.com>
2024-01-10 17:36:04 +0500

    This commit made only to share project with my teammates

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from news_app.views import *
+
 urlpatterns = [
+    path("", news_view, name="news_view_ht"),
+    path("news_django_page", main_page, name="news_django_page_ht")
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+def news_view (request):
+    news_list = News.objects.all()
+    context = {'news_list_ht': news_list}
+    # return render(request, 'html/index.html', context)
+    # return render(request, 'main.html', context)
+    return render(request, 'main.html', context)
+
+
+def main_page(request):
+    context = {'news_list_ht': news_list}
+
+    return render(request, 'news_django.html')
