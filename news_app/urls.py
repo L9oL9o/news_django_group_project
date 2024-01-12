afa6d2b7406eb972a2ba79c3760ed41377f230bb
l9o <ertye9020@gmail.com>
2024-01-13 00:59:41 +0500

    Bugs fixed, urls in html files

--- news_app/urls.py
+++ news_app/urls.py
@@ -14,11 +14,11 @@ from news_app.views import *
 
 
 urlpatterns = [
-    path("", main_news_page_view, name="main-page-news"),
-    path("main-news-page", main_news_page_view, name="main-page-news"),
+    path("", main_news_page_view, name="main-page-news-ht"),
+    path("main-news-page", main_news_page_view, name="main-page-news-ht"),
 
     # path('view/<path:news_path>/', main_news_page_view, name='main-page-news'),
     path("detail-news-page", detail_news_page, name="detail-news-page-ht"),
-    path("real/news/", from_real_news_view)
+    path("real-news", from_real_news_view, name="real-news-page-ht")
 
 ]
